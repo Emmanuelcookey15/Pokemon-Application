f01407dfa26bd7f1413dfc0dce1163f5dcc74ee6
user <user@users-MacBook-Pro.local>
2024-11-01 13:09:54 +0100

    implemented a custom search bar

--- presentation/src/main/java/com/example/presentation/ui/pokemon_list/PokemonListScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/pokemon_list/PokemonListScreen.kt
@@ -2,16 +2,33 @@ package com.example.presentation.ui.pokemon_list
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.presentation.R
@@ -29,7 +46,17 @@ fun PokemonListScreen(
             Spacer(modifier = Modifier.height(20.dp))
             Image(
                 painter = painterResource(id = R.drawable.ic_pokemon_logo),
-                contentDescription = stringResource(R.string.pokemon_logo)
+                contentDescription = stringResource(R.string.pokemon_logo),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            SearchBar(
+                hint = "Search...",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
             )
         }
     }
@@ -38,7 +65,47 @@ fun PokemonListScreen(
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier,
-    hint: String = ""
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
 ){
+    var text by remember {
+        mutableStateOf("")
+    }
 
+    var isHintDisplayed by remember {
+        mutableStateOf( hint != "")
+    }
+    
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.Black
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    //to hide the hint when type
+                    isHintDisplayed = it.isFocused
+                }
+        )
+
+        if (isHintDisplayed){
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
 }
